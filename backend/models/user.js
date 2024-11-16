6158ec2659f84f2d7fd73c2cabf7a8c1313ef019
Abijith27UK <abijithuk27@gmail.com>
2024-11-16 10:53:02 +0530

    Added signin and signup api, middleware and auth

---
+++ backend/models/user.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const { isEmail } = require('validator');
+//const bcrypt = require('bcrypt');
+
+const userSchema = mongoose.Schema({
+  name: {
+    required: true,
+    type: String,
+    trim: true,
+  },
+  school: {
+    required: true,
+    type: String,
+    trim: true,
+  },
+  email: {
+    unique: true,
+    required: true,
+    type: String,
+    trim: true,
+    validate: {
+      validator: (value) => {
+        const re =
+          /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+        return value.match(re);
+      },
+      message: "Please enter a valid email address",
+    },
+  },
+  password: {
+    required: true,
+    type: String,
+  },
+  token: { 
+    type: String 
+  },
+});
+
+// Hashing before password saved to db
+// userSchema.pre('save', async function(next) {
+//   const salt = await bcrypt.genSalt();
+//   this.password = await bcrypt.hash(this.password, salt);
+//   next();
+// });
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
